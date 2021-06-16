03ef2c492d4d023b35cd676245c2eae3415b448b
Dulaj Nadawa <dulajnadawa@gmail.com>
2021-06-16 20:51:30 +0530

    init

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cron = require('node-cron');
+const moment = require('moment');
+const admin = require('firebase-admin');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+dotenv.config();
+
+const serviceAccount = require('./serviceAccountKey.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
+const corsConfig = {
+    origin: "*",
+    methods: ["POST", "OPTIONS"],
+    credentials: true,
+    allowedHeaders: ["Content-Type"],
+    preflightContinue: true
+}
+
+app.post("/sendInit", cors(corsConfig), (req, res) => {
+    console.log("POST started");
+    const phone = req.body.phone;
+    const license = req.body.license;
+    const endTime = req.body.time;
+    const url = req.body.url;
+
+    console.log(phone);
+    console.log(license);
+    console.log(endTime);
+    console.log(url);
+
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = require('twilio')(accountSid, authToken);
+
+    try{
+        client.messages
+            .create({
+                body: 'Your vehicle is parked. License plate '+license+'. Your time will expire at '+endTime+'. Use following link to confirm parking '+url,
+                from: '+15407798532',
+                to: phone
+            })
+            .then((message) => {
+                console.log(message.sid);
+                res.send({'sid': message.sid, status: 'successful'});
+            }
+            );
+    }
+    catch (e) {
+        console.log(e);
+        res.send({status: "failed"});
+    }
+});
+
+
+app.post("/sendSchedule", cors(corsConfig), (req, res) => {
+    console.log(req.body.time);
+    const time = new Date(req.body.time);
+    const id = req.body.id;
+    console.log(time);
+
+    const sendTime = new Date(
+        moment()
+            .year(time.getUTCFullYear())
+            .month(time.getMonth())
+            .date(time.getUTCDate())
+            .hour(time.getUTCHours())
+            .minute(time.getUTCMinutes())
+            // .add(1, 'days')
+            // .subtract(15, 'minutes')
+            .add(3, 'minutes')
+            .format("yyyy/MM/DD HH:mm")
+        );
+
+    console.log(sendTime);
+
+    const task = cron.schedule(time.getUTCMinutes()+' '+time.getUTCHours()+' * * *', function () {
+        console.log('cron run');
+        const docRef = db.collection('parked').doc(id);
+        docRef.get().then(doc=>{
+            const isParked = doc.data().parked;
+            if(isParked){
+                ///rest of things
+                console.log("Currently parking");
+            }
+        });
+        task.stop();
+    });
+    res.send();
+});
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello</h1>");
+});
+
+app.listen(3000,() => console.log('Server started on port 3000'));

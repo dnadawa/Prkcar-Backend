8e6e74106892e92d29d0203425f7bacc1ba49399
Dulaj Nadawa <dulajnadawa@gmail.com>
2021-06-16 20:55:45 +0530

    Port changed

--- app.js
+++ app.js
@@ -100,4 +100,4 @@ app.get('/', (req, res) => {
     res.send("<h1>Hello</h1>");
 });
 
-app.listen(3000,() => console.log('Server started on port 3000'));
+app.listen(5000,() => console.log('Server started on port 3000'));

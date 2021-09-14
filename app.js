a2f7070af79f97f6357020884594b88c5da92e86
Dulaj Nadawa <dulajnadawa@gmail.com>
2021-09-14 21:42:27 +0530

    Change API for prkcar V2

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const schedule = require('node-schedule');
 const moment = require('moment');
 const admin = require('firebase-admin');
 const https = require('https');
-const fs = require('fs');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
 dotenv.config();
 
 const serviceAccount = require('./serviceAccountKey.json');
+
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
@@ -30,21 +30,17 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-app.post("/sendInit", cors(corsConfig), (req, res) => {
+app.post("/send", cors(corsConfig), (req, res) => {
     const phone = req.body.phone;
-    const license = req.body.license;
-    const endTime = req.body.time;
-    const url = req.body.url;
+    const messageBody = req.body.message;
 
     console.log(phone);
-    console.log(license);
-    console.log(endTime);
-    console.log(url);
 
     try{
         client.messages
             .create({
-                body: 'Your vehicle is parked. License plate '+license+'. Your time will expire at '+endTime+'. Use following link to confirm parking '+url,
+                // body: 'Your vehicle is parked. License plate '+license+'. Your time will expire at '+endTime+'. Use following link to confirm parking '+url,
+                body: messageBody,
                 from: '+15407798532',
                 to: phone
             })
@@ -59,7 +55,6 @@ app.post("/sendInit", cors(corsConfig), (req, res) => {
         res.send({status: "failed"});
     }
 });
-
 
 app.post("/sendSchedule", cors(corsConfig), (req, res) => {
     try{
@@ -83,9 +78,9 @@ app.post("/sendSchedule", cors(corsConfig), (req, res) => {
 
         schedule.scheduleJob(time, function () {
             console.log('CRON STARTED');
-            const docRef = db.collection('parked').doc(id);
+            const docRef = db.collection('parking').doc(id);
             docRef.get().then(doc=>{
-                const isParked = doc.data().parked;
+                const isParked = doc.data().status==='parked';
                 if(isParked){
                     console.log("PARKING");
                     try{
@@ -118,11 +113,48 @@ app.post("/sendSchedule", cors(corsConfig), (req, res) => {
 
 });
 
+app.post("/plateRecognize", cors(corsConfig), (request, response) => {
+    let message = {
+        'upload' : request.body.image.toString(),
+        'mmc': true
+    };
+
+    let headers = {
+        "Authorization": "Token "+process.env.PLATERECOGNIZER,
+        'Content-Type': 'application/json',
+    };
+
+    let options = {
+        host: "api.platerecognizer.com",
+        port: 443,
+        path: "/v1/plate-reader",
+        method: "POST",
+        headers: headers
+    };
+
+    let req = https.request(options, function(res) {
+        res.on('data', function(data) {
+            console.log("Response:");
+            console.log(JSON.parse(data));
+            response.send(JSON.parse(data));
+        });
+    });
+
+    req.on('error', function(e) {
+        console.log("ERROR:");
+        console.log(e);
+        response.send(e);
+    });
+
+    req.write(JSON.stringify(message));
+    req.end();
+});
+
 app.get('/', (req, res) => {
     res.send("<h1>Hello</h1>");
 });
 
-// app.listen(5000,() => console.log('Server started on port 5000'));
+// app.listen(3000, () => console.log('Server started'));
 
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.prkcar.com/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/api.prkcar.com/cert.pem', 'utf8');
